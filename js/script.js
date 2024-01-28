549d3c6f363c4a072887c523b2610f3c1f17ea41
“LeonDoungala22 <doungala.leon@gmail.com>
2024-01-28 15:13:02 +0100

    Template ajusted

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
-
-        $(document).ready(function () {
-            $("a").on('click', function (event) {
-                if (this.hash !== "") {
-                    event.preventDefault();
-                    var hash = this.hash;
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 800, function () {
-                        window.location.hash = hash;
-                    });
-                }
+$(document).ready(function () {
+    // Smooth scrolling for anchor links
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
             });
-        });
- 
+        }
+    });
+
+    // Update the navbar position on window scroll
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var navbarHeight = $(".navbar").outerHeight();
+
+        // Add a class to the navbar when scrolling down
+        if (scroll > navbarHeight) {
+            $(".navbar").addClass("fixed");
+        } else {
+            $(".navbar").removeClass("fixed");
+        }
+    });
+});
+
+
+// Navbar-specific JavaScript code
+
+// Add an event listener to highlight the navbar when scrolling
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('fixed');
+        } else {
+            navbar.classList.remove('fixed');
+        }
+    });
+});
